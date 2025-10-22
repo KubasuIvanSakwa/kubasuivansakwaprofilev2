f9419206e107a762aa8c445cf9c58fd668f4f37e
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2025-10-22 19:50:06 +0300

    chore: setting up react router 6, routing, icons, page creation

---
+++ src/Pages/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Socials from '../components/Socials'
+import Nav from '../components/Nav'
+import { Outlet } from 'react-router'
+
+function Layout() {
+  return (
+    <section className='bg-bgdark w-full h-[100vh] text-lighttext'>
+        <Socials />
+        <Nav />
+        <section>
+            <Outlet />
+        </section>
+    </section>
+  )
+}
+
+export default Layout
