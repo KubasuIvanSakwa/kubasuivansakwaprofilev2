e09932c2365ce3defc23bbd5ca014a75e941331d
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2025-10-24 12:44:59 +0300

    chore: hero, quote responsiveness

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,12 +8,12 @@ import Quote from '../assets/icons/quote.svg'
 function Home() {
   return (
     <section className='relative'>
-      <section className='mt-[5rem] flex'>
-        <section className='w-[40rem]'>
-          <p className='text-3xl fira-code-bold'>
+      <section className='mt-[5rem] flex lg:flex-row md:flex-row flex-col'>
+        <section className='lg:w-[40rem] md:w-[25rem]'>
+          <p className='text-3xl md:text-2xl fira-code-bold'>
             Kubasu Ivan is a <span className='text-darktext'>Mobile developer</span> and <span className='text-darktext'>Front-end Developer</span>.
           </p>
-          <p className='fira-code-light text-lg mt-3'>
+          <p className='fira-code-light lg:text-lg mt-3'>
             He crafts responsive and visually engaging websites and mobile apps where technology meets creativity.
           </p>
 
@@ -21,35 +21,35 @@ function Home() {
             <p className='fira-code-light text-lg'>Contact Me !!</p>
           </Link>
         </section>
-        <section className='-mt-[3rem] w-auto flex flex-col justify-center items-center relative z-20'>
+        <section className='lg:-mt-[3rem] md:-mt-[3rem] mt-[1.4rem] w-auto flex flex-col justify-center items-center relative z-20'>
           <img src={heroImage} alt="" className='grayscale w-[70%]'/>
           <img src={logoImage} alt="" className='grayscale absolute -z-10 left-[6rem] bottom-[4rem] w-[30%]'/>
           <img src={Dots} alt="" className='grayscale absolute z-10 right-[3rem] top-[2rem] w-[20%]'/>
           <Link className='border border-darktext w-fit p-1 flex justify-center items-center gap-2'>
-            <div className='w-[1.5rem] h-[1.5rem] bg-darktext'/>
-            <p className='fira-code-light text-lg'><span className='text-darktext'>Currently Working on </span>Mobile apps</p>
+            <div className='lg:w-[1.5rem] lg:h-[1.5rem] md:w-[1.5rem]  md:h-[1.5rem] w-[1rem] h-[1rem] bg-darktext'/>
+            <p className='fira-code-light lg:text-lg text-sm'><span className='text-darktext'>Currently Working on </span>Mobile apps</p>
           </Link>
         </section>
       </section>
 
-      <section className='w-full flex justify-center items-center mt-[5rem] relative flex-col'>
-        <section className='border border-darktext p-4 relative w-[37.3rem]'>
+      <section className='w-full flex items-center lg:mt-[5rem] mt-[5rem] relative flex-col '>
+        <section className='border border-darktext lg:p-4 md:p-4 pt-3 pb-3 pl-[0.4rem] pr-[0.4rem] relative lg:w-[37.3rem] md:w-[30rem] w-fit mt-[1rem]'>
           <div className='bg-bgdark absolute left-[1.4rem] -top-[1rem]'>
             <img src={Quote} alt=""/>
           </div>
 
-          <div className='text-2xl'>With great design comes great caffeine consumption</div>
+          <div className='lg:text-2xl md:text-lg text-[0.8rem]'>With great design comes great caffeine consumption</div>
 
           <div className='bg-bgdark absolute right-[1.4rem] -bottom-[1rem] rotate-180'>
            <img src={Quote} alt=""/>
         </div>
         </section>
-        <section className='w-[37.3rem] flex justify-end'>
-          <section className='w-[8rem] h-[3rem] border-b border-l border-r border-darktext relative flex justify-center items-center'>
+        <section className='lg:w-[37.3rem] md:w-[30rem] w-[19.6rem] flex justify-end'>
+          <section className='lg:w-[8rem] lg:h-[3rem] md:w-[8rem] md:h-[3rem] w-[6rem] h-[3rem] border-b border-l border-r border-darktext relative flex justify-center items-center'>
             <p>- Kubasu</p>
           </section>
         </section>
-      <div className='border border-darktext w-[10rem] h-[4rem] absolute -right-[7rem]'/>
+      <div className='border border-darktext w-[10rem] h-[4rem] absolute -right-[8rem] -top-[4rem]'/>
       </section>
 
     </section>
