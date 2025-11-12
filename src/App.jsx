cdebc9b82a36fb08a81d489517ce9df1fdf3560c
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2025-11-12 20:33:46 +0300

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import React, { Suspense, lazy }  from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router/dom'
 import Loading from './components/Loading'
+import { Analytics } from "@vercel/analytics/next" 
 
 
 function App() {
-const router = createBrowserRouter([
-  {
-    path:"/",
-    Component: lazy(() => import('./Pages/Layout')),
-    children: [
-      {index: true, Component: lazy(() => import('./Pages/Home'))},
-      {path: "projects", Component: lazy(() => import('./Pages/Projects'))},
-      {path: "meet-ivan", Component: lazy(() => import('./Pages/About'))},
-      {path: "contact", Component: lazy(() => import('./Pages/Contact'))},
-      {path: "*", Component: lazy(() => import('./Pages/NotFound'))},
-    ],
-  }
-])
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      Component: lazy(() => import('./Pages/Layout')),
+      children: [
+        {index: true, Component: lazy(() => import('./Pages/Home'))},
+        {path: "projects", Component: lazy(() => import('./Pages/Projects'))},
+        {path: "meet-ivan", Component: lazy(() => import('./Pages/About'))},
+        {path: "contact", Component: lazy(() => import('./Pages/Contact'))},
+        {path: "*", Component: lazy(() => import('./Pages/NotFound'))},
+      ],
+    }
+  ])
 
   return (
-    <Suspense fallback={<Loading />}>
-      <RouterProvider router={router} />
-    </Suspense>
+    // Use a React Fragment (<>) to wrap multiple top-level components
+    <>
+      <Suspense fallback={<Loading />}>
+        <RouterProvider router={router} />
+      </Suspense>
+      
+      {/* Add the Analytics component here */}
+      <Analytics />
+    </>
   )
 }
 
